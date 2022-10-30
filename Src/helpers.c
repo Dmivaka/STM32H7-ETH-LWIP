613a270d53d4045101b9a4c10caea0a72696665b
Dmitry <dmivaka@yandex.ru>
2022-10-30 17:19:32 +0600

    Bugfix in service bytes coding.

--- Src/helpers.c
+++ Src/helpers.c
@@ -4,7 +4,7 @@
 
 uint8_t decode_bus_num( uint32_t encoded_bus_num )
 {
-  return encoded_bus_num >> 30;
+  return encoded_bus_num >> 29;
 }
 
 uint32_t decode_can_id( uint32_t encoded_can_id )
@@ -15,7 +15,7 @@ uint32_t decode_can_id( uint32_t encoded_can_id )
 uint32_t encode_bus_id( uint8_t bus_num, uint32_t can_id )
 {
   uint32_t bus_id = can_id;
-  bus_id = ((bus_id & 0x1FFFFFFF) | (bus_num << 30));
+  bus_id = ((bus_id & 0x1FFFFFFF) | (bus_num << 29));
 
   return bus_id;
 }
